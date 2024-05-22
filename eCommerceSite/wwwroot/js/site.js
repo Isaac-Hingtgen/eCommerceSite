1597b646c8a07e75c787fdbd620a9221ab58713f
Isaac Hingtgen <ihingtge@asu.edu>
2024-05-22 15:19:41 -0700

    add sign in with google (needs some work)

--- eCommerceSite/wwwroot/js/site.js
+++ eCommerceSite/wwwroot/js/site.js
@@ -54,3 +54,29 @@ function addImageSpinners() {
 addImageSpinners();
 
 
+
+//var startApp = function () {
+//    gapi.load('auth2', function () {
+//        // Retrieve the singleton for the GoogleAuth library and set up the client.
+//        auth2 = gapi.auth2.init({
+//            client_id: '834917429572-05lbg6eo6u4c2d6jq57u2sopjj68o729.apps.googleusercontent.com',
+//            cookiepolicy: 'multi_host_origin',
+//            // Request scopes in addition to 'profile' and 'email'
+//            //scope: 'additional_scope'
+//        });
+//        attachSignin(document.getElementById('customBtn'));
+//    });
+//};
+
+//function attachSignin(element) {
+//    console.log(element.id);
+//    auth2.attachClickHandler(element, {},
+//        function (googleUser) {
+//            document.getElementById('name').innerText = "Signed in: " +
+//                googleUser.getBasicProfile().getName();
+//        }, function (error) {
+//            alert(JSON.stringify(error, undefined, 2));
+//        });
+//}
+
+
